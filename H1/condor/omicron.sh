3358684fc57e256e1b7b77969ef8328f2140e1ae
Hong-Yin Chen <hongyin.chen@ldas-pcdev11.ligo.caltech.edu>
2022-10-17 18:45:43 -0700

    Glitch generated

---
+++ H1/condor/omicron.sh
@@ -0,0 +1,21 @@
+# Job 015d0b274ac59c03b0fa5af4dcfbcc6d
+/home/hongyin.chen/miniconda3/envs/bbhnet-generate_glitches/bin/omicron 1263588388  1263588992  /home/hongyin.chen/Full_Dev/CCSNet/H1/parameters/parameters-0.txt  strict  
+
+# Job 976f827aae542ab3aa1093e01ce5e7b3
+/home/hongyin.chen/miniconda3/envs/bbhnet-generate_glitches/bin/omicron 1263588988  1263589592  /home/hongyin.chen/Full_Dev/CCSNet/H1/parameters/parameters-0.txt  strict  
+
+# Job e580e82b786d3da67205ae23d19f8286
+/home/hongyin.chen/miniconda3/envs/bbhnet-generate_glitches/bin/omicron 1263589588  1263590192  /home/hongyin.chen/Full_Dev/CCSNet/H1/parameters/parameters-0.txt  strict  
+
+# Job 85a296703df940c9378cf859967482ed
+/home/hongyin.chen/miniconda3/envs/bbhnet-generate_glitches/bin/omicron 1263590188  1263590792  /home/hongyin.chen/Full_Dev/CCSNet/H1/parameters/parameters-0.txt  strict  
+
+# Job a8fe155f7409812679db59b25267b682
+/home/hongyin.chen/miniconda3/envs/bbhnet-generate_glitches/bin/omicron 1263590788  1263591014  /home/hongyin.chen/Full_Dev/CCSNet/H1/parameters/parameters-0.txt  strict  
+
+# Job 0d9e5c0599bf9fc2f4e58149ed7f0d4f
+/usr/bin/bash -e  /home/hongyin.chen/Full_Dev/CCSNet/H1/condor/post-process-0-1263588388-1263591014.sh  
+
+# Job 8016a08a4793d302a3f93b3019aecb0c
+/home/hongyin.chen/Full_Dev/CCSNet/H1/condor/post-process-rm.sh 
+
---
+++ L1/condor/omicron.sh
@@ -0,0 +1,27 @@
+# Job 2a5fd63b265638180b0afdc7b7275e43
+/home/hongyin.chen/miniconda3/envs/bbhnet-generate_glitches/bin/omicron 1263588388  1263588992  /home/hongyin.chen/Full_Dev/CCSNet/L1/parameters/parameters-0.txt  strict  
+
+# Job c534e0e5002117f597c161df49d23c84
+/home/hongyin.chen/miniconda3/envs/bbhnet-generate_glitches/bin/omicron 1263588988  1263589592  /home/hongyin.chen/Full_Dev/CCSNet/L1/parameters/parameters-0.txt  strict  
+
+# Job 606619c0c92d4d999c779ed9a4bc782b
+/home/hongyin.chen/miniconda3/envs/bbhnet-generate_glitches/bin/omicron 1263589588  1263590192  /home/hongyin.chen/Full_Dev/CCSNet/L1/parameters/parameters-0.txt  strict  
+
+# Job 34a686739c2b06815d1fc3912110d35b
+/home/hongyin.chen/miniconda3/envs/bbhnet-generate_glitches/bin/omicron 1263590188  1263590792  /home/hongyin.chen/Full_Dev/CCSNet/L1/parameters/parameters-0.txt  strict  
+
+# Job f56b62cffbff9da728ccc905880c1d24
+/home/hongyin.chen/miniconda3/envs/bbhnet-generate_glitches/bin/omicron 1263590788  1263591392  /home/hongyin.chen/Full_Dev/CCSNet/L1/parameters/parameters-0.txt  strict  
+
+# Job c77a9671d5270e3186908c442755cb3d
+/home/hongyin.chen/miniconda3/envs/bbhnet-generate_glitches/bin/omicron 1263591388  1263591992  /home/hongyin.chen/Full_Dev/CCSNet/L1/parameters/parameters-0.txt  strict  
+
+# Job 061e73cabe9bde47e351ef54aa5bd6ae
+/home/hongyin.chen/miniconda3/envs/bbhnet-generate_glitches/bin/omicron 1263591988  1263592392  /home/hongyin.chen/Full_Dev/CCSNet/L1/parameters/parameters-0.txt  strict  
+
+# Job e31d0a1e644c7f62a45b0a7c2ca01f24
+/usr/bin/bash -e  /home/hongyin.chen/Full_Dev/CCSNet/L1/condor/post-process-0-1263588388-1263592392.sh  
+
+# Job 15666e8a1bb11c5bf77bb0cbed652a94
+/home/hongyin.chen/Full_Dev/CCSNet/L1/condor/post-process-rm.sh 
+
